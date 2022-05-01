44d5b1a7c4a5527828ea20469b3edb96cabeccfd
aymentaibi <aymentaibi662@gmail.com>
2022-05-01 02:20:00 +0100

    V3

--- app/src/main/java/com/example/projet_hotels/MainActivity.java
+++ app/src/main/java/com/example/projet_hotels/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.projet_hotels;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_login, btn_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_login = (Button) findViewById(R.id.login);
+        btn_register = (Button) findViewById(R.id.register);
 
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CreeUser.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
