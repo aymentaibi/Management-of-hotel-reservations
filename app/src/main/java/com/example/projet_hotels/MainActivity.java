f6593d4a01d02401dd9bde00e95f1cdf1b751747
aymentaibi <aymentaibi662@gmail.com>
2022-05-03 00:31:06 +0100

    V5

--- app/src/main/java/com/example/projet_hotels/MainActivity.java
+++ app/src/main/java/com/example/projet_hotels/MainActivity.java
@@ -1,30 +1,77 @@
 package com.example.projet_hotels;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_login, btn_register;
+
+    private EditText etmail,etpassword;
+    private String mail,password;
+    private String URL = "http://192.168.1.25/hotels/login.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        btn_login = (Button) findViewById(R.id.login);
-        btn_register = (Button) findViewById(R.id.register);
+        setContentView(R.layout.activity_main);
+        mail = password = "";
+        etmail = findViewById(R.id.etmail);
+        etpassword = findViewById(R.id.etpassword);
 
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CreeUser.class);
-                startActivity(intent);
-            }
-        });
     }
 
+    public void login(View fview) {
+        mail = etmail.getText().toString().trim();
+        password = etpassword.getText().toString().trim();
+        if(!mail.equals("") && !password.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
 
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("success")) {
+                        Intent intent = new Intent(MainActivity.this, MenuPricipale.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("failure")) {
+                        Toast.makeText(MainActivity.this, "Invalid Login ID/PASSWORD", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this,error.toString().trim(),Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> data = new HashMap<>();
+                    data.put("mail",mail);
+                    data.put("password",password);
+                    return data;
+                }
+
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+    } else{
+            Toast.makeText(this,"STP REMPLIRE LES CHAMPS",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- app/src/main/java/com/example/projet_hotels/MenuPricipale.java
+++ app/src/main/java/com/example/projet_hotels/MenuPricipale.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreeUser extends AppCompatActivity {
+public class MenuPricipale extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cree_user);
+        setContentView(R.layout.activity_menu_pricipale);
     }
 }
